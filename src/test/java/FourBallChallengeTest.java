dd9303afda0fe6dbe0eeb209a223f4bab2926c88
Shreya Wanisha <75380289+shreya2430@users.noreply.github.com>
2022-08-31 02:33:21 +0530

    Add files via upload

---
+++ src/test/java/FourBallChallengeTest.java
@@ -0,0 +1,12 @@
+import processing.core.PApplet;
+
+public class FourBallChallengeTest {
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge", args);
+
+        FourBallChallenge fourBallChallenge = new FourBallChallenge();
+//        fourBallChallenge.setDIAMETER(20);
+//        fourBallChallenge.setHEIGHT(480);
+//        fourBallChallenge.setWIDTH(640);
+    }
+}
